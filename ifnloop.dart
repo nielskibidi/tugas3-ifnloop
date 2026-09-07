e30731c0de71ae912dbf378ac7a92a1ccbbfa1ec
Nielskibidi <ngnielniel@gmail.com>
2026-09-07 20:42:42 +0700

    v1 - 07/09/2026

---
+++ ifnloop.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  // stdout.writeln('Masukan angka 1 Anda');
+  // var inputUser1 = stdin.readLineSync()!;
+  // stdout.writeln('Masukan angka 2 Anda');
+  // var inputUser2 = stdin.readLineSync()!;
+
+  // num hasil = num.parse(inputUser1) + num.parse(inputUser2);
+
+  // print('Ini hasil dari user $hasil')
+}
