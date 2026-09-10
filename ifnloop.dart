e1b9ce77ec7709a014c469ed89f0611aa5922b28
Nielskibidi <ngnielniel@gmail.com>
2026-09-10 12:40:09 +0700

    no. 2

--- ifnloop.dart
+++ ifnloop.dart
@@ -10,29 +10,48 @@ void main() {
 
   // print('Ini hasil dari user $hasil')
 
+  // =========================================================================================
+
   // No. 1 Menentukan nilai huruf dari angka 0–100
-  stdout.write('Masukkan nilai angka (0-100): ');
-  String? inputNilai = stdin.readLineSync();
+  // stdout.write('Masukkan nilai angka (0-100): ');
+  // String? inputNilai = stdin.readLineSync();
 
-  if (inputNilai != null && inputNilai.isNotEmpty) {
-    double nilai = double.parse(inputNilai);
-    String nilaiHuruf;
+  // if (inputNilai != null && inputNilai.isNotEmpty) {
+  //   double nilai = double.parse(inputNilai);
+  //   String nilaiHuruf;
 
-    if (nilai >= 85 && nilai <= 100) {
-      nilaiHuruf = 'A';
-    } else if (nilai >= 70 && nilai < 85) {
-      nilaiHuruf = 'B';
-    } else if (nilai >= 55 && nilai < 70) {
-      nilaiHuruf = 'C';
-    } else if (nilai >= 0 && nilai < 55) {
-      nilaiHuruf = 'D';
+  //   if (nilai >= 85 && nilai <= 100) {
+  //     nilaiHuruf = 'A';
+  //   } else if (nilai >= 70 && nilai < 85) {
+  //     nilaiHuruf = 'B';
+  //   } else if (nilai >= 55 && nilai < 70) {
+  //     nilaiHuruf = 'C';
+  //   } else if (nilai >= 0 && nilai < 55) {
+  //     nilaiHuruf = 'D';
+  //   } else {
+  //     nilaiHuruf = 'Nilai tidak valid';
+  //   }
+
+  //   print('Grade: $nilaiHuruf');
+  // } else {
+  //   print('Input tidak boleh kosong!');
+  // }
+
+  // print('\n');
+
+  // =========================================================================================
+
+  // 2. Menampilkan bilangan 1–20
+  for (int i = 1; i <= 20; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      print('$i: FizzBuzz');
+    } else if (i % 3 == 0) {
+      print('$i: Fizz');
+    } else if (i % 5 == 0) {
+      print('$i: Buzz');
     } else {
-      nilaiHuruf = 'Nilai tidak valid';
+      print('$i');
     }
-
-    print('Grade: $nilaiHuruf');
-  } else {
-    print('Input tidak boleh kosong!');
   }
 
   print('\n');
